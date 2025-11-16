c9897a118a35799581c04f7f5b36792f76647f8c
Jennifer Marques <jenniferbrito@alu.ufc.br>
2025-11-16 19:33:46 -0300

    script e css de options

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onIntalled.addListener(() => {
+    console.log('Extensão de Relatórios Pedagógicos instalada');
+
+chrome.storage.sync.get(['apiKey', 'turma', 'idade', 'professora', 'periodoPadrao'], (result) => {
+    if(!result.apiKey) {
+        chrome.runtime.openOptionsPage();
+        }
+    });    
+
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request.action === 'openOptions') {
+        chrome.runtime.openOptionsPage();
+    }
+
+});
